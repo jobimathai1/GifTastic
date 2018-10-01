2b19b03a1f8685f7b0edae8615b532619c87c8de
Jobi Mathai <jobi.mathai@gmail.com>
2018-09-30 20:08:58 -0700

    fixed bugs with submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,27 @@
-$(document).ready(function(){
-    var displayedButtons = ["cat", "dog", "birds"];
+//load the javascript once HTML document is finished loading.
+$(document).ready(function () {
+    //Default buttons to display when the page loads
+    var displayedButtons = ["tacos", "cats", "bloopers"];
 
-    function displayImg(){
+    //Function to display the images from Giphy API onto the page.
+    function displayImg() {
         $("#display-images").empty();
         var input = $(this).attr("data-name");
         var limit = 10;
+
+        //Query the Giphy API based on the user's input. 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=t0gOHu8czKpdjDuhsD1vHDyFgnyPuXkY";
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).done(function(response){
-            for (var i = 0; i < limit; i++){
+        }).done(function (response) {
+            for (var i = 0; i < limit; i++) {
+
+                //Create a new div on the page to display the Gifs
                 var displayDiv = $("<div>");
                 displayDiv.addClass("holder");
 
+                //Create an img url based on the data provided by the Giphy API.
                 var image = $("<img>");
                 image.attr("src", response.data[i].images.original_still.url);
                 image.attr("data-still", response.data[i].images.original_still.url);
@@ -23,44 +31,46 @@ $(document).ready(function(){
                 displayDiv.append(image);
 
                 var rating = response.data[i].rating;
-                console.log(response);
-                var pRating = $("<p>").text("Rating: "+ rating);
+                var pRating = $("<p>").text("Rating: " + rating);
                 displayDiv.append(pRating);
                 $("#display-images").append(displayDiv);
             }
         });
     }
 
-    function renderBtns(){
+    //After the user enters a word in the search field, create a button to display on the page
+    function renderBtns() {
         $("#display-buttons").empty();
 
-        for (var j = 0; j<displayedButtons.length; j++){
+        for (var j = 0; j < displayedButtons.length; j++) {
 
-            var newBtn = $("<button>");
+            var newBtn = $("<button>")
             newBtn.attr("class", "btn btn-default");
-            newBtn.attr("id", "input");
+            newBtn.attr("id", "input")
             newBtn.attr("data-name", displayedButtons[j]);
             newBtn.text(displayedButtons[j]);
             $("#display-buttons").append(newBtn);
         }
     }
 
-    function imgChangeState(){
+    //a function to change the state of the image from Still to Animate & back to Still when the image is clicked on by the user
+    function imgChangeState() {
         var state = $(this).attr("data-state");
         var animateImg = $(this).attr("data-animate");
         var stillImg = $(this).attr("data-still");
 
-        if(state == "still"){
+        if (state == "still") {
             $(this).attr("src", animateImg);
             $(this).attr("data-state", "animate");
         }
-        else if(state == "animate") {
+        else if (state == "animate") {
             $(this).attr("src", stillImg);
             $(this).attr("data-state", "still");
         }
     }
 
-    $("#submitBtn").on("Click", function(){
+    //Creating a new button for each word entered by the users
+    $("#submitBtn").on("click", function () {
         var input = $("#user-input").val().trim();
         form.reset();
         displayedButtons.push(input);
@@ -68,9 +78,13 @@ $(document).ready(function(){
 
         return false;
     })
-
+    //Calling the renderBtns function 
     renderBtns();
+
+    //When the button is clicked, this displays the gifs
     $(document).on("click", "#input", displayImg);
+
+    //When the user clicks on a gif, it changes the state from still to animate & vice versa
     $(document).on("click", ".gif", imgChangeState);
 
 });
